b7fca7e1f71e4f77da8fb8bc26f00b99fd8919ca
Alex <alxkohh@gmail.com>
2020-10-27 23:07:04 +0800

    Add users controller

---
+++ backend/src/controller/users.js
@@ -0,0 +1,25 @@
+const pool = require('../db');
+
+async function createUsersTable(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+async function dropUsersTable(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
