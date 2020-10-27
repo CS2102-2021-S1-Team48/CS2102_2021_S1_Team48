1f7c0a2132731ab4cfb6b54b5267ed0afc397421
Alex <alxkohh@gmail.com>
2020-10-27 23:33:39 +0800

    Complete users controller and routes

--- backend/src/controller/users.js
+++ backend/src/controller/users.js
@@ -52,3 +52,23 @@ async function changeUsername(ctx) {
     }
 }
 
+// PATCH api at router
+async function changePassword(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+module.exports = {
+    createUsersTable,
+    dropUsersTable,
+    createUser,
+    changeUsername,
+    changePassword
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,24 @@
 const Router = require('koa-router');
 const sayHello = require('./controller/hello');
-const { createPetTable, insertPetRow, readAllPetRows, dropPetTable } = require('./controller/pet');
+const { createUsersTable, dropUsersTable, createUser, changeUsername, changePassword } = require('./controller/users');
 
 const router = new Router();
 
 router.get('/hello', sayHello);
 
+// users
+router.post('/users/createtable', createUsersTable);
+router.del('/users/droptable', dropUsersTable);
+router.post('/users', createUser);
+router.patch('/users/changeusername/:newusername', changeUsername);
+router.patch('/users/changepassword/:newpassword', changePassword);
 
-// deprecated
-// router.post('/createPetTable', createPetTable);
-// router.post('/insertPetRow/:petname', insertPetRow);
-// router.get('/readAllPetRows', readAllPetRows);
-// router.del('/dropPetTable', dropPetTable);
+
+/* deprecated routes
+router.post('/createPetTable', createPetTable);
+router.post('/insertPetRow/:petname', insertPetRow);
+router.get('/readAllPetRows', readAllPetRows);
+router.del('/dropPetTable', dropPetTable);
+*/
 
 module.exports = router;
