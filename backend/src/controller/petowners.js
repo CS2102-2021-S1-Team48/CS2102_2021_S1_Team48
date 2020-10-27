1c6ce2175f05860e7adfac66f2d0516c0b923e24
Alex <alxkohh@gmail.com>
2020-10-27 23:52:24 +0800

    Add petowners controller and routes

---
+++ backend/src/controller/petowners.js
@@ -0,0 +1,32 @@
+const pool = require('../db');
+
+// POST api at router
+async function createPetownersTable(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// DEL api at router
+async function dropPetownersTable(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+module.exports = {
+    createPetownersTable,
+    dropPetownersTable
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,10 +1,13 @@
 const Router = require('koa-router');
 const sayHello = require('./controller/hello');
+const { createPetownersTable, dropPetownersTable } = require('./controller/petowners');
 const { createUsersTable, dropUsersTable, createUser, changeUsername, changePassword } = require('./controller/users');
 
 const router = new Router();
 
 router.get('/hello', sayHello);
+
+// routes categorised by controllers
 
 // users
 router.post('/users/createtable', createUsersTable);
@@ -12,6 +15,13 @@ router.del('/users/droptable', dropUsersTable);
 router.post('/users', createUser);
 router.patch('/users/changeusername/:newusername', changeUsername);
 router.patch('/users/changepassword/:newpassword', changePassword);
+
+// petowners
+router.post('/petowners/createtable', createPetownersTable);
+router.del('/petowners/droptable', dropPetownersTable);
+
+
+
 
 
 /* deprecated routes
