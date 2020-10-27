3c76b619d5eecd724980ed6e39625c21c06c37f9
Alex <alxkohh@gmail.com>
2020-10-28 02:32:31 +0800

    Add caretakersft controller and routes

---
+++ backend/src/controller/caretakersft.js
@@ -0,0 +1,114 @@
+const pool = require('../db');
+
+// POST api at router
+async function createCaretakersFtTable(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// DEL api at router
+async function dropCaretakersFtTable(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// POST api at router
+async function switchCaretakerPtToFt(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// GET api at router
+async function getCaretakerFtInfo(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// PATCH api at router
+async function editStartDate1(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// PATCH api at router
+async function editEndDate1(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// PATCH api at router
+async function editStartDate2(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// PATCH api at router
+async function editEndDate2(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+module.exports = {
+    createCaretakersFtTable,
+    dropCaretakersFtTable,
+    editStartDate1,
+    editEndDate1,
+    editStartDate2,
+    editEndDate2
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,6 +7,7 @@ const { createCaretakersPtTable, dropCaretakersPtTable } = require('./controller
 const { createAvailabilitiesTable, dropAvailabilitiesTable, postAvailability, getAvailabilities, editAvailability, deleteAvailability } = require('./controller/availabilities');
 const { createBaseDailyPricesTable, dropBaseDailyPricesTable, addBaseDailyPrice, getBaseDailyPrices, editBaseDailyPrice, deleteBaseDailyPrice } = require('./controller/basedailyprices');
 const { createBidsTable, dropBidsTable, addBid, getAcceptedBids, getUnacceptedBids, getBids, getReviewsOfCaretaker, acceptBid, undoAcceptBid, deleteBid } = require('./controller/bids');
+const { createCaretakersFtTable, dropCaretakersFtTable, editStartDate1, editEndDate1, editStartDate2, editEndDate2 } = require('./controller/caretakersft');
 
 
 
@@ -39,6 +40,15 @@ router.get('/caretakers/:usernamect', getCaretakerByUsername);
 // caretakerspt
 router.post('/caretakers/createtable', createCaretakersPtTable);
 router.del('/caretakers/droptable', dropCaretakersPtTable);
+
+// caretakersft
+router.post('/caretakers/createtable', createCaretakersFtTable);
+router.del('/caretakers/droptable', dropCaretakersFtTable);
+router.get('/caretakersft', getCaretakerFtInfo);
+router.patch('/caretakersft/startdate1/:startdate1', editStartDate1);
+router.patch('/caretakersft/enddate1/:enddate1', editEndDate1);
+router.patch('/caretakersft/startdate2/:startdate2', editStartDate2);
+router.patch('/caretakersft/enddate2/:enddate2', editEndDate2);
 
 // availabilities
 router.post('/availabilities/createtable', createAvailabilitiesTable);
