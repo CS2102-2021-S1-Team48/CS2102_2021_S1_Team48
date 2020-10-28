9ac83f527f0f10c0ab3d4ecdf9b11d13b5bcc387
JustinnT <e0311076@u.nus.edu>
2020-10-28 20:02:52 +0800

    add GET apis for pet controller and route

--- backend/src/controller/pets.js
+++ backend/src/controller/pets.js
@@ -26,7 +26,50 @@ async function dropPetsTable(ctx) {
     }
 }
 
+// POST /pets?petname=eva&pettype=cat&requirements=aircon
+// POST api at router
+async function addPet(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// GET api at router
+async function getPet(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// GET api at router
+async function getPetByPetname(ctx) {
+    try {
+        const sqlQuery = '';
+        await pool.query(sqlQuery);
+        ctx.body = 'success';
+    } catch (e) {
+        console.log(e);
+        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
 module.exports = {
     createPetsTable,
-    dropPetsTable
+    dropPetsTable,
+    addPet,
+    getPet,
+    getPetByPetname
 };
