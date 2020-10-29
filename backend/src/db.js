124a47b36e99bea79d0f9137273df76e12cfad3b
Alex <alxkohh@gmail.com>
2020-10-29 22:17:23 +0800

    Change db connection

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,17 +1,17 @@
 const { Pool } = require('pg');
 
-const elephantUrl = 'postgres://haikibbc:qeAUubYqybNnfAIuVi5RlglDsMtAaB2d@john.db.elephantsql.com:5432/haikibbc';
-
-const pool = new Pool({
-    connectionString: elephantUrl
-});
+// const elephantUrl = 'postgres://haikibbc:qeAUubYqybNnfAIuVi5RlglDsMtAaB2d@john.db.elephantsql.com:5432/haikibbc';
 
 // const pool = new Pool({
-//     user: 'postgres',
-//     host: 'petdb.cluuodrt5y8f.ap-southeast-1.rds.amazonaws.com',
-//     database: 'postgres',
-//     password: 'postgres',
-//     port: 5432,
+//     connectionString: elephantUrl
 // });
+
+const pool = new Pool({
+    user: 'postgres',
+    host: 'petdb.cluuodrt5y8f.ap-southeast-1.rds.amazonaws.com',
+    database: 'postgres',
+    password: 'postgres',
+    port: 5432,
+});
 
 module.exports = pool;
