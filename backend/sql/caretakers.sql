0fe82648004eed7d96c77e464279d275472cae39
Alex <alxkohh@gmail.com>
2020-10-29 21:22:29 +0800

    Add sql files

---
+++ backend/sql/caretakers.sql
@@ -0,0 +1,27 @@
+/* Creates a caretaker table /*
+
+CREATE TABLE caretakers (
+    username varchar(20) PRIMARY KEY,
+    petdays INTEGER DEFAULT 0,
+    rating NUMERIC DEFAULT NULL,
+    petlimit INTEGER,
+
+    FOREIGN KEY(username) REFERENCES users(username)
+	ON DELETE CASCADE
+	ON UPDATE CASCADE
+);
+
+/* Drops a user table /*
+
+DROP TABLE IF EXISTS caretakers
+
+/* Retrieves names of all caretakers /*
+
+SELECT *
+  FROM caretakers
+
+/* Retrieves details of a specific caretakers /*
+
+SELECT *
+  FROM caretakers
+ WHERE username = 'ExampleName'
