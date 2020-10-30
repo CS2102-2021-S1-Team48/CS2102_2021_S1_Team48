8cef6b2ff9721fc5642c0c993065147a0fc630c5
JustinnT <e0311076@u.nus.edu>
2020-10-30 15:21:35 +0800

    update api for basedailyprices controller

--- backend/src/controller/basedailyprices.js
+++ backend/src/controller/basedailyprices.js
@@ -28,9 +28,13 @@ async function dropBaseDailyPricesTable(ctx) {
 
 // POST /basedailyprices?amount=123&pettype=dog&minrating=3
 // POST api at router
+// NOTE: to test need to have adminusername. I added a query parameter for it i.e. ?adminusername=123. Remove when using cookies
 async function addBaseDailyPrice(ctx) {
+    const { amount, pettype, minrating } = ctx.query;
+
+    const adminUsername = ctx.query.adminusername;
     try {
-        const sqlQuery = '';
+        const sqlQuery = `INSERT INTO basedailyprices VALUES (${amount}, '${pettype}', ${minrating}, '${adminUsername}')`;
         await pool.query(sqlQuery);
         ctx.body = 'success';
     } catch (e) {
@@ -44,32 +48,64 @@ async function addBaseDailyPrice(ctx) {
 // GET /basedailyprices?pettype=cat&minrating=5 
 // GET api at router
 async function getBaseDailyPrices(ctx) {
-    try {
-        const sqlQuery = '';
-        await pool.query(sqlQuery);
-        ctx.body = 'success';
-    } catch (e) {
-        console.log(e);
-        ctx.body = 'error';
-        ctx.status = 403;
+    const { pettype, minrating } = ctx.query;
+    if (pettype === undefined && minrating === undefined) {
+        try {
+            const sqlQuery = 'SELECT * FROM basedailyprices';
+            const resultObject = await pool.query(sqlQuery);
+            ctx.body = 'success';
+            console.table(resultObject.rows);
+        } catch (e) {
+            console.log(e);
+            ctx.body = 'error';
+            ctx.status = 403;
+        }
+    } else if (pettype === undefined) {
+        try {
+            const sqlQuery = `SELECT * FROM basedailyprices WHERE minrating = '${minrating}'`;
+            const resultObject = await pool.query(sqlQuery);
+            ctx.body = 'success';
+            console.table(resultObject.rows);
+        } catch (e) {
+            console.log(e);
+            ctx.body = 'error';
+            ctx.status = 403;
+        }
+    } else if (minrating === undefined) {
+        try {
+            const sqlQuery = `SELECT * FROM basedailyprices WHERE pettype = '${pettype}'`;
+            const resultObject = await pool.query(sqlQuery);
+            ctx.body = 'success';
+            console.table(resultObject.rows);
+        } catch (e) {
+            console.log(e);
+            ctx.body = 'error';
+            ctx.status = 403;
+        }
+    } else {
+        try {
+            const sqlQuery = `SELECT * FROM basedailyprices WHERE pettype = '${pettype}' AND minrating = '${minrating}'`;
+            const resultObject = await pool.query(sqlQuery);
+            ctx.body = 'success';
+            console.table(resultObject.rows);
+        } catch (e) {
+            console.log(e);
+            ctx.body = 'error';
+            ctx.status = 403;
+        }
     }
 }
 
 // PATCH /basedailyprices/:pettype/:minrating?amount=3
 // PATCH api at router
+// See note at function addBaseDailyPrice
 async function editBaseDailyPrice(ctx) {
+    const { pettype, minrating } = ctx.params;
+    const amount = ctx.query.amount;
     
-    // traditional way
-    // const pettype = ctx.params.pettype;
-    // const minrating = ctx.params.minrating;
-    // const amount = ctx.query.amount;
-
-    // js way. destructuring an object
-    // const { pettype, minrating } = ctx.params;
-    // const { amount } = ctx.query;
-
+    const adminUsername = ctx.query.adminusername;
     try {
-        const sqlQuery = '';
+        const sqlQuery = `UPDATE basedailyprices SET amount = ${amount}, username_admin = '${adminUsername}' WHERE pettype = '${pettype}' AND minrating = ${minrating}`;
         await pool.query(sqlQuery);
         ctx.body = 'success';
     } catch (e) {
@@ -79,10 +115,12 @@ async function editBaseDailyPrice(ctx) {
     }
 }
 
+// DEL /basedailyprices/:pettype/:minrating , deleteBaseDailyPrice
 // DEL api at router
 async function deleteBaseDailyPrice(ctx) {
+    const { pettype, minrating } = ctx.params;
     try {
-        const sqlQuery = '';
+        const sqlQuery = `DELETE FROM basedailyprices WHERE pettype = '${pettype}' AND minrating = ${minrating}`;
         await pool.query(sqlQuery);
         ctx.body = 'success';
     } catch (e) {
