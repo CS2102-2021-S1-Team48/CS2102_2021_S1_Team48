653c1a22dbdc6756281e47c64781c7f7c1de4396
JustinnT <e0311076@u.nus.edu>
2020-11-01 12:14:56 +0800

    update sql for bids

--- backend/sql/bids.sql
+++ backend/sql/bids.sql
@@ -21,7 +21,7 @@ CREATE TABLE bids (
 
 -- TRIGGER AFTER INSERT IF CARETAKER = FULLTIMER -> AUTO ACCEPT WHEN PETLIMIT UNDER HIM NOT REACHED
 CREATE OR REPLACE FUNCTION checkFullTime() RETURNS TRIGGER AS 
-' BEGIN IF NEW.username_caretaker IN (SELECT username FROM caretakers_pt) THEN NEW.accepted := False; ELSIF (SELECT petlimit FROM caretakers c WHERE NEW.username_caretaker = c.username) > (SELECT COUNT(*) FROM bids WHERE startdate = NEW.startdate AND enddate = NEW.enddate AND accepted = True) THEN NEW.accepted := True; END IF; RETURN NEW; END; ' 
+' BEGIN IF NEW.username_caretaker IN (SELECT username FROM caretakers_pt) THEN NEW.accepted := False; ELSIF (SELECT petlimit FROM caretakers c WHERE NEW.username_caretaker = c.username) > (SELECT COUNT(*) FROM bids WHERE startdate = NEW.startdate AND enddate = NEW.enddate AND accepted = True AND username_caretaker = NEW.username_caretaker) THEN NEW.accepted := True; END IF; RETURN NEW; END; ' 
 LANGUAGE plpgsql;
 
 CREATE TRIGGER insertBids
@@ -31,7 +31,7 @@ FOR EACH ROW EXECUTE PROCEDURE checkFullTime();
 
 -- TRIGGER BEFORE UPDATE IF ACCEPTED = TRUE -> CHECK IF CARETAKER IS PARTIMER. IF PARTIMER CHECK PETLIMIT AND HOW MANY PETS IS UNDER HIS CARE IN THE BIDS TABLE
 CREATE OR REPLACE FUNCTION checkPartTime() RETURNS TRIGGER AS 
-' BEGIN IF NEW.accepted = False THEN NEW.accepted = False; ELSIF NEW.accepted = True AND NEW.username_caretaker IN (SELECT username FROM caretakers_pt) AND (SELECT petlimit FROM caretakers c WHERE NEW.username_caretaker = c.username) > (SELECT COUNT(*) FROM bids WHERE startdate = NEW.startdate AND enddate = NEW.enddate AND OLD.accepted = True) THEN NEW.accepted = True; ELSE raise EXCEPTION ''pet limit reached''; END IF; RETURN NEW; END; ' 
+' BEGIN IF NEW.accepted = False THEN NEW.accepted = False; ELSIF (SELECT petlimit FROM caretakers c WHERE NEW.username_caretaker = c.username) > (SELECT COUNT(*) FROM bids WHERE startdate = NEW.startdate AND enddate = NEW.enddate AND accepted = True AND username_caretaker = NEW.username_caretaker) THEN NEW.accepted = True; ELSE raise EXCEPTION ''pet limit reached''; END IF; RETURN NEW; END; ' 
 LANGUAGE plpgsql;
 
 CREATE TRIGGER updateBids
@@ -39,18 +39,18 @@ BEFORE UPDATE ON bids
 FOR EACH ROW EXECUTE PROCEDURE checkPartTime();
 
 
--- RIGGER AFTER INSERT INTO REVIEWS -> UPDATE TABLE CARETAKERS ON RATING
+-- TRIGGER AFTER INSERT INTO REVIEWS -> UPDATE TABLE CARETAKERS ON RATING
 CREATE OR REPLACE FUNCTION updateRating() RETURNS TRIGGER AS
 ' DECLARE r INTEGER; 
 BEGIN SELECT AVG(bids.rating) INTO r FROM bids WHERE bids.username_caretaker = NEW.username_caretaker AND bids.rating IS NOT NULL; 
-IF r IS NULL THEN r = 2; END IF; 
+IF r IS NULL THEN r = 3; END IF; 
 UPDATE caretakers SET rating = r WHERE username = NEW.username_caretaker;
 RETURN NEW; 
 END;'
 LANGUAGE plpgsql;
 
 CREATE TRIGGER updateRating
-BEFORE UPDATE ON bids
+AFTER UPDATE ON bids
 FOR EACH ROW EXECUTE PROCEDURE updateRating();
 
 
@@ -59,7 +59,7 @@ FOR EACH ROW EXECUTE PROCEDURE updateRating();
 DROP TABLE bids;
 
 -- Add a new bid into the table
--- POST /bids?transfermethod=deliver&paymentmethod=123&petname=emma&username_caretake=Duc&startdate=27102020&enddate=28102020 , addBid
+-- POST /bids?transfermethod=deliver&paymentmethod=123&petname=emma&username_caretake=Duc&startdate=27102020&enddate=28102020&pettype=dog , addBid
 INSERT INTO bids VALUES ('pickup', 'cash', 'sam', 'johndoe9999', 'johndoe99', '2020-10-31', '2020-11-01', 'dog');
 
 -- Get all accepted bids from the table
