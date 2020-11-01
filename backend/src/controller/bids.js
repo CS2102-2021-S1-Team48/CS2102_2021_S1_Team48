5e753e9dc56f38d5d42b5b0f61838f7c3f74342f
JustinnT <e0311076@u.nus.edu>
2020-11-01 12:23:37 +0800

    update api for bids controller

--- backend/src/controller/bids.js
+++ backend/src/controller/bids.js
@@ -26,11 +26,15 @@ async function dropBidsTable(ctx) {
     }
 }
 
-// POST /bids?transfermethod=deliver&paymentmethod=123&petname=emma&username_caretake=Duc&startdate=27102020&enddate=28102020
+// POST /bids?transfermethod=deliver&paymentmethod=123&petname=emma&username_caretake=Duc&startdate=27102020&enddate=28102020&pettype=dog
 // POST api at router
+// Same issue, need petowner username. Temp solution is to use query param for petowner username
 async function addBid(ctx) {
+    const { transfermethod, paymentmethod, petname, username_caretaker, startdate, enddate, pettype } = ctx.query;
+
+    const username_petowner = ctx.query.username_petowner;
     try {
-        const sqlQuery = '';
+        const sqlQuery = `INSERT INTO bids VALUES ('${transfermethod}', '${paymentmethod}', '${petname}', '${username_petowner}', '${username_caretaker}', '${startdate}', '${enddate}', '${pettype}')`;
         await pool.query(sqlQuery);
         ctx.body = 'success';
     } catch (e) {
@@ -43,8 +47,14 @@ async function addBid(ctx) {
 // GET api at router
 async function getAcceptedBids(ctx) {
     try {
-        const sqlQuery = '';
-        await pool.query(sqlQuery);
+        const sqlQuery = 'SELECT * FROM bids WHERE accepted = True';
+        const resultobject = await pool.query(sqlQuery);
+
+        // Documented bug here, PGadmin shows the correct date but when returned below,
+        // it will show a different date probably due to timezone conversion
+        // See availabilities.js line 55
+        // This holds true for any return type that is of date.
+        console.table(resultobject.rows);
         ctx.body = 'success';
     } catch (e) {
         console.log(e);
@@ -56,8 +66,9 @@ async function getAcceptedBids(ctx) {
 // GET api at router
 async function getUnacceptedBids(ctx) {
     try {
-        const sqlQuery = '';
-        await pool.query(sqlQuery);
+        const sqlQuery = 'SELECT * FROM bids WHERE accepted = False';
+        const resultobeject = await pool.query(sqlQuery);
+        console.table(resultobeject.rows);
         ctx.body = 'success';
     } catch (e) {
         console.log(e);
@@ -66,13 +77,14 @@ async function getUnacceptedBids(ctx) {
     }
 }
 
-// GET /bids?petname=eva&usernamect=john&usernamepo=lim
-// If there is no ? query then it should get all bids
+// GET /bids?petname=eva&usernamect=john&usernamepo=lim , getBids
 // GET api at router
 async function getBids(ctx) {
+    const { petname, usernamect, usernamepo } = ctx.query;
     try {
-        const sqlQuery = '';
-        await pool.query(sqlQuery);
+        const sqlQuery = `SELECT * FROM bids WHERE petname = '${petname}' AND username_caretaker = '${usernamect}' AND username_petowner = '${usernamepo}'`;
+        const resultobject = await pool.query(sqlQuery);
+        console.table(resultobject.rows);
         ctx.body = 'success';
     } catch (e) {
         console.log(e);
