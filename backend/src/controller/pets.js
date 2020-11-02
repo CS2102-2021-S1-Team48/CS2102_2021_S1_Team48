42cf6fe50f530fe09eabd94672156066a59364cf
JustinnT <e0311076@u.nus.edu>
2020-11-03 01:59:58 +0800

    update pets controller

--- backend/src/controller/pets.js
+++ backend/src/controller/pets.js
@@ -5,10 +5,11 @@ async function createPetsTable(ctx) {
     try {
         const sqlQuery = '';
         await pool.query(sqlQuery);
-        ctx.body = 'success';
+        ctx.body = {
+            'success': 'True!'
+        };
     } catch (e) {
         console.log(e);
-        ctx.body = 'error';
         ctx.status = 403;
     }
 }
@@ -18,31 +19,35 @@ async function dropPetsTable(ctx) {
     try {
         const sqlQuery = '';
         await pool.query(sqlQuery);
-        ctx.body = 'success';
+        ctx.body = {
+            'success': 'True!'
+        };
     } catch (e) {
         console.log(e);
-        ctx.body = 'error';
         ctx.status = 403;
     }
 }
 
-// POST /pets?petname=eva&pettype=cat&requirements=aircon
+// POST /pets/petowner1?petname=eva&pettype=cat&requirements=aircon
 // POST api at router
-// Same issue, need username of the person who is going to add the pet. Temporary solution use query param ?username=123
 async function addPet(ctx) {
     const { petname, pettype } = ctx.query;
     var requirements = ctx.query.requirements;
 
-    const username = ctx.query.username;
+    const usernamepo = ctx.params.usernamepo;
 
     requirements = (requirements === undefined) ? 'NIL' : requirements;
     try {
-        const sqlQuery = `INSERT INTO pets VALUES ('${petname}', '${pettype}', '${requirements}', '${username}')`;
+        const sqlQuery = `INSERT INTO pets VALUES ('${petname}', '${pettype}', '${requirements}', '${usernamepo}')`;
         await pool.query(sqlQuery);
-        ctx.body = 'success';
+        ctx.body = {
+            'success': 'True!',
+            'requirements': requirements,
+            'petname': petname,
+            'pettype': pettype
+        };
     } catch (e) {
         console.log(e);
-        ctx.body = 'error';
         ctx.status = 403;
     }
 }
@@ -52,84 +57,111 @@ async function getPet(ctx) {
     try {
         const sqlQuery = 'SELECT * FROM pets';
         const resultObject = await pool.query(sqlQuery);
-        ctx.body = 'success';
-        console.table(resultObject.rows);
+        const row = resultObject.rows;
+        ctx.body = {
+            'pets': row
+        };
+        console.table(row);
     } catch (e) {
         console.log(e);
-        ctx.body = 'error';
         ctx.status = 403;
     }
 }
 
 // GET api at router
-// GET /pets/:petname , getPetByPetname
-// Same issue, need username of the person going to find his own pet. Temporary solution use query param ?username=123
-async function getPetByPetname(ctx) {
-    const petname = ctx.params.petname;
-
-    const username = ctx.query.username;
+// Newly added
+async function getPetByUsername(ctx) {
+    const usernamepo = ctx.params.usernamepo;
     try {
-        const sqlQuery = `SELECT * FROM PETS WHERE petname = '${petname}' AND username_petowner = '${username}'`;
+        const sqlQuery = `SELECT * FROM pets WHERE username_petowner = ${usernamepo}`;
         const resultObject = await pool.query(sqlQuery);
-        ctx.body = 'success';
-        console.table(resultObject.rows);
+        const row = resultObject.rows;
+        ctx.body = {
+            'pets': row
+        };
+        console.table(row);
     } catch (e) {
         console.log(e);
-        ctx.body = 'error';
+        ctx.status = 403;
+    }
+}
+
+// GET api at router
+// GET /pets/:petname/:usernamepo , getPetByPetname
+async function getPetByPetname(ctx) {
+    const { petname, usernamepo } = ctx.params;
+    try {
+        const sqlQuery = `SELECT * FROM pets WHERE petname = '${petname}' AND username_petowner = '${usernamepo}'`;
+        const resultObject = await pool.query(sqlQuery);
+        const row = resultObject.rows;
+        ctx.body = {
+            'pets': row
+        };
+        console.table(row);
+    } catch (e) {
+        console.log(e);
         ctx.status = 403;
     }
 }
 
 // PATCH api at router
-// PATCH /pets/:petname?petname=evaline&requirements=coldaircon , editPetDetails
-// Same issue, need username of the person going to edit own pet.
+// PATCH /pets/:petname/:usernamepo?petname=evaline&requirements=coldaircon , editPetDetails
 
 // UPDATE pets SET petname = 'evaline' WHERE petname = 'emma' AND username_petowner = 'johndoe99';
 // UPDATE pets SET requirements = 'hugs daily' WHERE petname = 'emma2' AND username_petowner = 'johndoe99';
 // UPDATE pets SET petname = 'evaline', requirements = 'carnivore' WHERE petname = 'emma' AND username_petowner = 'johndoe999';
 async function editPetDetails(ctx) {
-    const currentPetName = ctx.params.petname;
-    const { petname, requirements } = ctx.query;
+    const currentpetname = ctx.params.petname;
+    const usernamepo = ctx.params.usernamepo;
 
-    const username = ctx.query.username;
+    const { petname, requirements } = ctx.query;
     try {
         if (requirements === undefined) {
-            const sqlQuery = `UPDATE pets SET petname = '${petname}' WHERE petname = '${currentPetName}' AND username_petowner = '${username}'`;
+            const sqlQuery = `UPDATE pets SET petname = '${petname}' WHERE petname = '${currentpetname}' AND username_petowner = '${usernamepo}'`;
             await pool.query(sqlQuery);
-            ctx.body = 'success';
+            ctx.body = {
+                'success': 'True!',
+                'petname': petname
+            };
             
         } else if (petname === undefined) {
-            const sqlQuery = `UPDATE pets SET requirements = '${requirements}' WHERE petname = '${currentPetName}' AND username_petowner = '${username}'`;
+            const sqlQuery = `UPDATE pets SET requirements = '${requirements}' WHERE petname = '${currentpetname}' AND username_petowner = '${usernamepo}'`;
             await pool.query(sqlQuery);
-            ctx.body = 'success';
+            ctx.body = {
+                'success': 'True!',
+                'requirements': requirements
+            };
 
         } else {
-            const sqlQuery = `UPDATE pets SET petname = '${petname}', requirements = '${requirements}' WHERE petname = '${currentPetName}' AND username_petowner = '${username}'`;
+            const sqlQuery = `UPDATE pets SET petname = '${petname}', requirements = '${requirements}' WHERE petname = '${currentpetname}' AND username_petowner = '${usernamepo}'`;
             await pool.query(sqlQuery);
-            ctx.body = 'success';
+            ctx.body = {
+                'success': 'True!',
+                'requirements': requirements,
+                'petname': petname
+            };
 
         }
     } catch (e) {
         console.log(e);
-        ctx.body = 'error';
         ctx.status = 403;
     }
 }
 
 // DEL api at router
-// DEL /pet/:petname , deletePetByPetname
-// Same issue, need username of the person going to delete own pet. Temp solution use query param
+// DEL /pet/:petname/:usernamepo , deletePetByPetname
 async function deletePetByPetname(ctx) {
-    const petname = ctx.params.petname;
-
-    const username = ctx.query.username;
+    const { petname, usernamepo } = ctx.params;
     try {
-        const sqlQuery = `DELETE FROM pets WHERE username_petowner = '${username}' AND petname = '${petname}'`;
+        const sqlQuery = `DELETE FROM pets WHERE username_petowner = '${usernamepo}' AND petname = '${petname}'`;
         await pool.query(sqlQuery);
-        ctx.body = 'success';
+        ctx.body = {
+            'success': 'True!',
+            'petname': petname,
+            'usernamepo': usernamepo
+        };
     } catch (e) {
         console.log(e);
-        ctx.body = 'error';
         ctx.status = 403;
     }
 }
@@ -138,6 +170,7 @@ module.exports = {
     createPetsTable,
     dropPetsTable,
     addPet,
+    getPetByUsername,
     getPet,
     getPetByPetname,
     editPetDetails,
