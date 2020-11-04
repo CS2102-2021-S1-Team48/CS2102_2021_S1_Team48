83da647beb9e7537e0aea2b16131c5644504bcfd
unknown <yeodonghan@gmail.com>
2020-11-05 06:19:26 +0800

    added requests for get availability and bid. api for search availability to be updated and api for add bids to be fixed

--- frontend/src/SearchCaretaker.svelte
+++ frontend/src/SearchCaretaker.svelte
@@ -1,40 +1,64 @@
 <script>
-  import { createEventDispatcher } from "svelte";
+  import { createEventDispatcher, onMount } from "svelte";
+  import { account } from "./user.js";
+
+  let username;
 
   let dispatch = createEventDispatcher();
 
-  let begin;
-  let end;
-  let name;
-  let payment = "Credit Card";
+  let begin = "";
+  let end = "";
+  let petobj;
+  //let payment = "Credit Card";
+  let petsjson = [];
 
-  let pets = [
-    { name: "Maple", category: "Corgi", requirements: "NIL", id: 1 },
-    {
-      name: "Rax",
-      category: "T-Rex",
-      requirements: "Daily teeth brushing",
-      id: 2,
-    },
-    {
-      name: "Morty",
-      category: "Corgi",
-      requirements: "Daily walks",
-      id: 3,
-    },
-  ];
+  const unsubscribe = account.subscribe((value) => {
+    username = value;
+  });
 
-  let options = ["Credit Card", "Cash"];
+  onMount(async () => {
+    await fetch(`http://18.139.110.246:3000/pets/${username}`, {
+      method: "GET",
+    })
+      .then((resp) => resp.json())
+      .then((data) => (petsjson = data.pets));
+
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+      dd = "0" + dd;
+    }
+    if (mm < 10) {
+      mm = "0" + mm;
+    }
+
+    today = yyyy + "-" + mm + "-" + dd;
+    document.getElementById("datefield").setAttribute("min", today);
+    document.getElementById("datefield2").setAttribute("min", today);
+  });
 
   const handleSubmit = () => {
     //console.log(begin, end, name, payment);
-    const request = {
-      start: begin,
-      end: end,
-      name: name,
-      payment: payment,
-    };
-    dispatch("SearchCareTaker", request);
+    if (begin === "") {
+      alert("Please enter start date!");
+    } else if (end === "") {
+      alert("Please enter end date!");
+    } else if (petobj[0] === "none") {
+      alert("Please select pet");
+    } else if (begin > end) {
+      alert("Starting date cannot be after ending date!");
+    } else {
+      const request = {
+        start: begin,
+        end: end,
+        name: petobj[0],
+        category: petobj[1],
+        //payment: payment,
+      };
+      dispatch("SearchCareTaker", request);
+    }
   };
 </script>
 
@@ -91,9 +115,8 @@
         <input
           class="short-input"
           type="date"
-          id="start"
-          min="2020-10-29"
-          max="2030-12-31"
+          id="datefield"
+          min="1899-01-01"
           bind:value={begin} />
       </div>
       <div class="form-input">
@@ -101,16 +124,18 @@
         <input
           class="short-input"
           type="date"
-          id="end"
-          min="2020-10-29"
-          max="2030-12-31"
+          id="datefield2"
+          min="1899-01-01"
           bind:value={end} />
       </div>
       <div class="form-input">
         <label for="pets">Your Pet</label>
-        <select class="short-input" id="pets" bind:value={name}>
-          {#each pets as pet}
-            <option value={pet.name}>{pet.name}</option>
+        <select class="short-input" id="pets" bind:value={petobj}>
+          <option value={['none', 'none']} selected disable hidden>
+            Select a Pet
+          </option>
+          {#each petsjson as pet}
+            <option value={[pet.petname, pet.pettype]}>{pet.petname}</option>
           {/each}
         </select>
       </div>
