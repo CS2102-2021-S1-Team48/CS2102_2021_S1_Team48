e1703311061f10cff919435fdece3337adf8173d
Alex <alxkohh@gmail.com>
2020-11-07 21:53:10 +0800

    Fix switchCaretakerPtToFt

--- backend/src/controller/caretakerspt.js
+++ backend/src/controller/caretakerspt.js
@@ -6,10 +6,20 @@ async function getEligibilityToBeFT(ctx) {
 
     try {
         const sqlQuery = `SELECT 
-                            CASE WHEN EXISTS (SELECT 1 FROM (SELECT COUNT(*) cnt FROM (SELECT DISTINCT enddate-startdate+1 AS days FROM availabilities WHERE username_caretaker = '${usernamect}') blocklengths WHERE days >= 150) c WHERE c.cnt = 2)
+                            CASE WHEN EXISTS ((SELECT 1 FROM (SELECT COUNT(*) cnt FROM (
+                                    SELECT DISTINCT enddate-startdate+1 AS days 
+                                    FROM availabilities 
+                                    WHERE username_caretaker = '${usernamect}') blocklengths 
+                                    WHERE days >= 150) c WHERE c.cnt = 2) UNION 
+                                    (SELECT 1 
+                                    FROM (SELECT count(*) 
+                                    FROM (SELECT DISTINCT enddate-startdate+1 AS availdays 
+                                    FROM availabilities WHERE username_caretaker = '${usernamect}') as c
+                                    WHERE availdays >= 300) AS n 
+                                    WHERE n.count = 1))
                                 THEN 'eligible'
                                 ELSE 'not eligible'
-                            END;`;
+                            END`;
 
         const resultObject = await pool.query(sqlQuery);
         const rows = resultObject.rows;
